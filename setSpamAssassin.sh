0c43370a63a285adfe743a35aefaaa33ab700e8f
root <root@frodo.(none)>
2011-09-13 00:24:38 -0300

    arquivo configuracao spamassassin

---
+++ setSpamAssassin.sh
@@ -0,0 +1,14 @@
+# Criado em: 2011/08/23 (Ter) 20:35:40 (BRT)
+# Ultima Modificacao: 2011/08/23 (Ter) 20:35:40 (BRT)
+# Autor: Rafael Dutra <raffaeldutra@gmail.com>
+# http://www.du3x.com
+
+# habilitando para iniciar automaticamente
+function setSpamAssassin()
+{
+
+echo "Habilitando o spamassassin"
+sleep 2
+sed -i "s/ENABLED=0/ENABLED=1/" /etc/default/spamassassin
+
+}
